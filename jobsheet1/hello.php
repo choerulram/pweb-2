9ad0191b1fe89d6b782d4a63c2e0ded5289d8f5e
Choerul <achmadchoerul12345@gmail.com>
2023-08-28 07:56:24 +0700

    ubah hello

--- jobsheet1/hello.php
+++ jobsheet1/hello.php
@@ -1,3 +1,20 @@
-<?php 
-    echo "<em>Hello World!</em>"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+    <form action="" method="post">
+        <div class="container border w-25 p-5 mt-25 bg-light rounded">
+            <?php 
+                echo "<em>Hello World!</em>"
+            ?>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
