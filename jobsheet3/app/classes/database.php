684fca8156d1cf5cc04ef01e8dda16e3f1835c86
Choerul <achmadchoerul12345@gmail.com>
2023-08-29 08:04:58 +0700

    add edit_dsn

--- jobsheet3/app/classes/database.php
+++ jobsheet3/app/classes/database.php
@@ -44,10 +44,21 @@
             return $hasil;
         }
 
+        function edit_dsn($id) {
+            $data = mysqli_query($this->koneksi, "select * from dosen where id = '$id'");
+            while ($d=mysqli_fetch_array($data)) {
+                $hasil[] = $d;
+            }
+            return $hasil;
+        }
+
         function update($id, $nim, $nama, $alamat) {
             mysqli_query($this->koneksi, "update mhs set nim='$nim', nama='$nama', alamat='$alamat' where id='$id'");
         }
 
+        function update_dsn($id, $nidn, $nama, $alamat) {
+            mysqli_query($this->koneksi, "update dosen set nidn='$nidn', nama='$nama', alamat='$alamat' where id='$id'");
+        }
 
         function hapus($id) {
             mysqli_query($this->koneksi, "delete from mhs where id='$id'");
--- jobsheet3/app/views/proses_dsn.php
+++ jobsheet3/app/views/proses_dsn.php
@@ -6,5 +6,8 @@
     if ($aksi=="tambah") {
         $db->tambah_dsn($_POST['nidn'],$_POST['nama'],$_POST['alamat']);
         header("location:tampil_dsn.php");
-    } 
+    } else if ($aksi=="update") {
+        $db->update_dsn($_POST['id'],$_POST['nidn'],$_POST['nama'],$_POST['alamat']);
+        header("location:tampil_dsn.php");
+    }
 ?>
