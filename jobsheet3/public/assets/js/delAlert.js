bf63cde1ee191bf95f6d7b485fa5fb8e470c74a5
Choerul <achmadchoerul12345@gmail.com>
2023-08-30 13:03:16 +0700

    add comment

--- jobsheet3/public/assets/js/delAlert.js
+++ jobsheet3/public/assets/js/delAlert.js
@@ -1,8 +1,13 @@
+// hapus alert
 function removeAlert() {
+    //  mengambil elemen dengan "success-alert"
     var alertDiv = document.getElementById("success-alert");
+
+    // jika elemen alert ada 
     if (alertDiv) {
       alertDiv.remove();
     }
   }
   
+  // set waktu dijalankan adalah 1500 milidetik (1,5 detik)
   setTimeout(removeAlert, 1500);
