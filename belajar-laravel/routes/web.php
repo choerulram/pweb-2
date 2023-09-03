f0f12fe74a1ab48f52b66885b2de66547035cbcf
Choerul <achmadchoerul12345@gmail.com>
2023-09-03 22:27:02 +0700

    route/routes/routing laravel

--- belajar-laravel/routes/web.php
+++ belajar-laravel/routes/web.php
@@ -16,3 +16,59 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return 'Hello World';
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['name' => 'Achmad Choerul', 'no' => '08645748']);
+});
+
+// Route::view('/contact', 'contact', ['name' => 'Achmad Choerul', 'no' => '08645748']);
+
+
+// redirect routes
+Route::redirect('/contact', '/contact-us');
+
+Route::get('/product', function () {
+    return 'product';
+});
+
+// Route::get('/product/{id}', function ($id) {
+//     return 'Ini adalah product dengan id ' . $id;
+// });
+
+Route::get('/product/{id}', function ($id) {
+    return view('product.detail', ['id' => $id]);
+});
+
+
+// route prefixes
+Route::prefix('administrator')->group(function () {
+    Route::get('/about-admin', function() {
+        return 'about admin';
+    });
+
+    Route::get('/profil-admin', function() {
+        return 'profil admin';
+    });
+
+    Route::get('/contact-admin', function() {
+        return 'contact admin';
+    });
+
+    Route::get('/about-admin2', function() {
+        return 'about admin2';
+    });
+
+    Route::get('/profil-admin2', function() {
+        return 'profil admin2';
+    });
+
+    Route::get('/contact-admin2', function() {
+        return 'contact admin2';
+    });
+});
+
+
